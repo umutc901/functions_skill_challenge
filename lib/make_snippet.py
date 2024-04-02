a0d29346116b3a77fdf44df4ef3897621f25f59b
Umut <umutc901@gmail.com>
2024-04-02 15:50:39 +0100

    make_snippets file created

---
+++ lib/make_snippet.py
@@ -0,0 +1,2 @@
+def make_snippet(text):
+    pass
---
+++ tests/test_make_snippet.py
@@ -0,0 +1,6 @@
+from lib.make_snippet import *
+
+def if_empty_string_return_empty_string():
+    result = make_snippet("")
+    assert result == ""
+    
