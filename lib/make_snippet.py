473b4303605978f50b55ee4dfb138cbd9cd6bb07
Umut <umutc901@gmail.com>
2024-04-02 16:19:39 +0100

    worked with writing tests for a function

--- lib/make_snippet.py
+++ lib/make_snippet.py
@@ -1,2 +1,12 @@
 def make_snippet(text):
-    pass
+    words = text.split(" ")
+    if len(words) >5:
+         first_five = words[:5]
+         snippet = " ".join(first_five)
+         return snippet + "..."
+    else:
+        return text
+
+def count_words(string):
+    word = string.split(" ")
+    return len(word)
--- tests/test_make_snippet.py
+++ tests/test_make_snippet.py
@@ -1,6 +1,16 @@
 from lib.make_snippet import *
 
-def if_empty_string_return_empty_string():
+def test_if_empty_string_return_empty_string():
     result = make_snippet("")
     assert result == ""
-    
+
+def test_five_word_string():
+    result = make_snippet("one two three four five")
+    assert result == "one two three four five"
+def test_six_word_string():
+    result = make_snippet("one two three four five six")
+    assert result == "one two three four five..."
+
+def test_count_words():
+    result = count_words("one two three four five six seven")
+    assert result == 7
