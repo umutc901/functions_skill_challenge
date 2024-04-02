fca7814272c7854e70d2d61eda5f9407c3a5ce1a
Umut <umutc901@gmail.com>
2024-04-02 17:22:58 +0100

    furhter testing and debugging practice

--- lib/make_snippet.py
+++ lib/make_snippet.py
@@ -10,3 +10,10 @@ def make_snippet(text):
 def count_words(string):
     word = string.split(" ")
     return len(word)
+
+def reading_time_estimate(wordcount):
+    time = wordcount/200
+    return time
+
+def grammar_check(grammar):
+    return grammar[0].isupper() and grammar[-1] in "!.?"
